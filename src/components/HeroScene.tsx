747241a70293234a8528a7b11a5aaee135b95970
ppal36491-prog <ppal36491@gmail.com>
2026-05-22 10:54:05 +0530

    Adjust scaling and margin of HeroScene component

--- src/components/HeroScene.tsx
+++ src/components/HeroScene.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export function HeroScene() {
   return (
-    <div className="w-full h-full overflow-hidden rounded-3xl scale-125 ml-12">
+    <div className="w-full h-full overflow-hidden rounded-3xl scale-100 sm:scale-110 lg:scale-125 ml-0 sm:ml-6 lg:ml-12">
     <Spline scene="https://prod.spline.design/kGgp06tXkIXSUhKs/scene.splinecode" />
     </div>
   );
